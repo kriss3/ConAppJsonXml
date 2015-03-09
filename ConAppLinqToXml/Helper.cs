ad1ff81b08332f9ab4c2eac1d6c8a3c3ec5829d1
kriss3 <kris_s2@outlook.com>
2015-03-09 16:52:05 -0700

    Adding comments and code explanations

--- ConAppLinqToXml/Helper.cs
+++ ConAppLinqToXml/Helper.cs
@@ -22,11 +22,13 @@ namespace ConAppLinqToXml
 			Location = ConfigurationManager.AppSettings["location"];
 			XDocument myDoc = XDocument.Load(Location+fileName);
 
+			//query
 			var names = (from rec in myDoc.Descendants("Person")
 								let id = int.Parse(rec.Attribute("id").Value)
 								select new Person(id, rec.Element("Name").Value,
 								int.Parse(rec.Element("Age").Value), rec.Element("Job").Value)).ToList();
-
+			
+			//displaying results in console application
 			foreach (var item in names)
 			{
 				Console.WriteLine(item.Print());
